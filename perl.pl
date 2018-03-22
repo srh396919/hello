a38c02f60a9ab8338a430664a322ad93ce893d1e
srh396919 <srh396919@gmail.com>
2018-03-22 14:17:25 +0530

    First commit

---
+++ perl.pl
@@ -0,0 +1,4 @@
+$input = "Hai, How are you ";
+print "Original line :$input \n";
+$input =~ tr/a/o/;
+print "New line : $input  ";
---
+++ perl1.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+$input="hai, how are you ";
+print "Original line :$input\n";
+$input =~ s/hai/hello/;
+print "New line : $input  ";
+
